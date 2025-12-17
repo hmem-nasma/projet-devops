a4a90a6e3f68d53e24148e3851557820758c187d
Nasma Hmem <hmemnasma@gmail.com>
2025-12-17 23:37:37 +0100

    test fix

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,19 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
 @SpringBootTest
 class StudentManagementApplicationTests {
-
+    
+    // Mock la DataSource pour éviter la connexion réelle à MySQL
+    @MockBean
+    private DataSource dataSource;
+    
     @Test
     void contextLoads() {
+        // Test vide - vérifie juste que le contexte Spring démarre
     }
-
 }
